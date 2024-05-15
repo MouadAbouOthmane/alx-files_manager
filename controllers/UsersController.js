ba1a78bceed831028729cade76883022a443df0f
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-05-15 15:15:55 +0000

    4. Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 import crypto from 'crypto';
 import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
 
 class UsersController {
     static async postNew(req, res) {
@@ -30,6 +31,28 @@ class UsersController {
         const userId = result.insertedId;
 
         res.status(201).json({ id: userId, email });
+    }
+
+    static async getMe(req, res) {
+        const token = req.headers['x-token'];
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const key = `auth_${token}`;
+        const userId = await redisClient.get(key);
+
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const user = await dbClient.db.collection('users').findOne({ _id: new dbClient.ObjectId(userId) });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        return res.status(200).json({ id: user._id, email: user.email });
     }
 }
 
