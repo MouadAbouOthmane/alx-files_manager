683553904c24860db1013f40d41fc2ddd68e9d80
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-05-15 16:41:45 +0000

    7. File publish/unpublish

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,7 +2,7 @@ import dbClient from '../utils/db.js';
 import redisClient from '../utils/redis.js';
 
 class FilesController {
-    static async getShow(req, res) {
+    static async putPublish(req, res) {
         const { id } = req.params;
         const token = req.headers['x-token'];
 
@@ -16,23 +16,26 @@ class FilesController {
             return res.status(404).json({ error: 'Not found' });
         }
 
-        return res.status(200).json(file);
+        await dbClient.db.collection('files').updateOne({ _id: dbClient.ObjectId(id) }, { $set: { isPublic: true } });
+        return res.status(200).json({ ...file, isPublic: true });
     }
 
-    static async getIndex(req, res) {
+    static async putUnpublish(req, res) {
+        const { id } = req.params;
         const token = req.headers['x-token'];
-        const { parentId = '0', page = '0' } = req.query;
 
         const userId = await FilesController.getUserId(token);
         if (!userId) {
             return res.status(401).json({ error: 'Unauthorized' });
         }
 
-        const perPage = 20;
-        const skip = parseInt(page) * perPage;
+        const file = await dbClient.db.collection('files').findOne({ _id: dbClient.ObjectId(id), userId });
+        if (!file) {
+            return res.status(404).json({ error: 'Not found' });
+        }
 
-        const files = await dbClient.db.collection('files').find({ userId, parentId }).skip(skip).limit(perPage).toArray();
-        return res.status(200).json(files);
+        await dbClient.db.collection('files').updateOne({ _id: dbClient.ObjectId(id) }, { $set: { isPublic: false } });
+        return res.status(200).json({ ...file, isPublic: false });
     }
 
     static async getUserId(token) {
